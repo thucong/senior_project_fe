eeadf0baaf3d22b5dc552b631d852b2b29c02ee7
thucong <51808157+thucong@users.noreply.github.com>
2022-03-19 11:38:42 +0700

    fix appointment

--- src/components/user/ConsultationItem.js
+++ src/components/user/ConsultationItem.js
@@ -8,122 +8,217 @@ import EditConsultation from "./EditConsultation";
 import DeleteConsultation from "./DeleteConsultation";
 import ConsultationService from "../../services/ConsultationService";
 
-
 const cookies = new Cookies();
 class ConsultationItem extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       consultation: [],
       time_consultation: "",
-      info_consultation: '',
+      info_consultation: "",
       list_consultation: "",
       list_schedule: "",
-      choice_delete: '',
-      hours:'',
-      minutes:'',
-      seconds:''
-    }
+      choice_delete: "",
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+      isMounted: false,
+      isShowCallButton: false,
+    };
   }
-  componentDidMount(){
-    axios.get(API_URL + "consultation/patient/" + cookies.get('id_user'),{  headers: { Authorization: `Bearer ${cookies.get("token")}` }}).then((res) => {
-      this.setState({consultation: res.data})
-    })
-
+  componentDidMount() {
+    axios
+      .get(API_URL + "consultation/patient/" + cookies.get("id_user"), {
+        headers: { Authorization: `Bearer ${cookies.get("token")}` },
+      })
+      .then((res) => {
+        this.setState({ consultation: res.data });
+      });
+    this.setState({ isMounted: true });
   }
   onChoose = (id) => {
-    axios.get(API_URL + "consultation/" + id, {  headers: { Authorization: `Bearer ${cookies.get("token")}` }}).then((res) => {
-        this.setState({info_consultation: res.data[0]});
-    })
-    window.$('#editConsultation').modal('show');
-  }
+    axios
+      .get(API_URL + "consultation/" + id, {
+        headers: { Authorization: `Bearer ${cookies.get("token")}` },
+      })
+      .then((res) => {
+        this.setState({ info_consultation: res.data[0] });
+      });
+    window.$("#editConsultation").modal("show");
+  };
   onChooseDelete = (id) => {
-      window.$('#deleteConsultation').modal('show');
-      this.setState({choice_delete: id})
-  }
+    window.$("#deleteConsultation").modal("show");
+    this.setState({ choice_delete: id });
+  };
   countdown = (time) => {
-  
     var time_consultation = new Date(time).getTime();
-      setInterval(() => {
-        var now = new Date().getTime();
-        var D = time_consultation - now
-        if( D < 0){
-          clearInterval()
-        }else{
-          var hours = Math.floor(D/(1000*60*60))
-          var minutes = Math.floor(D/(1000*60))
-          var seconds = Math.floor(D/1000)
-          hours %=24
-        minutes %=60
-        seconds %=60
-        this.setState({hours: hours})
-        this.setState({minutes: minutes})
-        this.setState({seconds: seconds})
-        }
-       
-   
-      
-    },1000)
-   
-    
-    
-  }
+    setInterval(() => {
+      var now = new Date().getTime();
+      var D = time_consultation - now;
+      if (D < 0) {
+        clearInterval();
+        this.setState({
+          isShowCallButton: true,
+        });
+      } else {
+        var hours = Math.floor(D / (1000 * 60 * 60));
+        var minutes = Math.floor(D / (1000 * 60));
+        var seconds = Math.floor(D / 1000);
+        hours %= 24;
+        minutes %= 60;
+        seconds %= 60;
+        this.setState({ hours: hours });
+        this.setState({ minutes: minutes });
+        this.setState({ seconds: seconds });
+      }
+    }, 1000);
+  };
   call = (link) => {
-    window.open(link,"_blank")
-  }
+    window.open(link, "_blank");
+  };
   changeStatus = (id_consultation) => {
     let id = id_consultation;
-    let status = 'done';
+    let status = "done";
     ConsultationService.updateStatus(id, status).then((res) => {
       window.location.reload();
-    })
-  }
+    });
+  };
 
   render() {
-    const {consultation} = this.state;
-   // var now = new Date().getTime();
-    //console.log(now)
+    const { consultation } = this.state;
+    // var now = new Date().getTime();
 
     return (
       <div className="col col-lg-8 mt-5 pr-5 ">
         <h3 className="h3 border-bottom mb-3 left-text">Các cuộc tư vấn</h3>
-        {consultation.length > 0 ? (consultation.map((item, index) => (
-          <div className="row mb-3 consultation p-3" key={index} >
-          <div className="col col-2">
-            <img className="mx-auto " src={item.doctorId.avatar} height="150" width="150" alt="" />
-          </div>
-          <div className="col col-10 info-consultation pl-4">
-            <p>Tên bác sĩ: <span className="info-item">{item.doctorId.fullname}</span></p>
-            <p>Ngày: <span className="info-item">{item.date}</span></p>
-            <p>Thời gian: <span className="info-item">{Moment(item.start).format(' hh:mm a')} - {Moment(item.end).format(' hh:mm a')}</span></p>
-            <p>Lý do của cuộc tư vấn: <span className="info-item">{item.content}</span></p>
-            <p>Trạng thái: <span className={item.status === 'waiting' ? 'wait' : '' || item.status === 'reject' ? 'reject' : '' || item.status === 'confirm'||'done' ? 'confirm': ''}>{item.status.toUpperCase()}</span></p>
-            {item.status === "done" ? <Link className="send-comment" to={"/doctor/" + item.doctorId._id}>Gửi phản hồi cho bác sĩ</Link> : ""}
-            {item.status === "waiting" ? (
-                 <div className="mt-2">
-                 <button className="btn btn-success" onClick={(e) => this.onChoose(item._id)}>Sửa</button>&ensp;
-                 <button className="btn btn-danger" onClick={(e) => this.onChooseDelete(item._id)}>Xóa</button>
-               </div>
-            ) : ""}
-               
-            {item.status === "confirmed" ? (
-              <div>
-                <div>
-                  <p>Ghi chú: <span className="info-item">{item.note}</span></p>
+        {consultation.length > 0
+          ? consultation.map((item, index) => (
+              <div className="row mb-3 consultation p-3" key={index}>
+                <div className="col col-2">
+                  <img
+                    className="mx-auto "
+                    src={item.doctorId.avatar}
+                    height="150"
+                    width="150"
+                    alt=""
+                  />
                 </div>
-                {(new Date().getTime() < new Date(item.start).getTime()) ? this.countdown(item.start) : ''}
-                {(new Date().getTime() > new Date(item.end).getTime()) ? this.changeStatus(item._id) : ''}
-               {(this.state.hours > 0) || (this.state.minutes > 0) || (this.state.seconds > 0) ? (
-                  <p>Thời gian còn lại cho đến cuộc tư vấn: <span  className="info-item">{this.state.hours}:{this.state.minutes}:{this.state.seconds} </span></p>
-                ) :  <button className="btn btn-success mt-1" onClick={(e) => this.call(item.linkCall)}>Bắt đầu cuộc gọi</button>} 
+                <div className="col col-10 info-consultation pl-4">
+                  <p>
+                    Tên bác sĩ:{" "}
+                    <span className="info-item">{item.doctorId.fullname}</span>
+                  </p>
+                  <p>
+                    Ngày: <span className="info-item">{item.date}</span>
+                  </p>
+                  <p>
+                    Thời gian:{" "}
+                    <span className="info-item">
+                      {Moment(item.start).format(" hh:mm a")} -{" "}
+                      {Moment(item.end).format(" hh:mm a")}
+                    </span>
+                  </p>
+                  <p>
+                    Lý do của cuộc tư vấn:{" "}
+                    <span className="info-item">{item.content}</span>
+                  </p>
+                  <p>
+                    Trạng thái:{" "}
+                    <span
+                      className={
+                        item.status === "waiting"
+                          ? "wait"
+                          : "" || item.status === "reject"
+                          ? "reject"
+                          : "" || item.status === "confirm" || "done"
+                          ? "confirm"
+                          : ""
+                      }
+                    >
+                      {item.status.toUpperCase()}
+                    </span>
+                  </p>
+                  {item.status === "done" ? (
+                    <Link
+                      className="send-comment"
+                      to={"/doctor/" + item.doctorId._id}
+                    >
+                      Gửi phản hồi cho bác sĩ
+                    </Link>
+                  ) : (
+                    ""
+                  )}
+                  {item.status === "waiting" ? (
+                    <div className="mt-2">
+                      <button
+                        className="btn btn-success"
+                        onClick={(e) => this.onChoose(item._id)}
+                      >
+                        Sửa
+                      </button>
+                      &ensp;
+                      <button
+                        className="btn btn-danger"
+                        onClick={(e) => this.onChooseDelete(item._id)}
+                      >
+                        Xóa
+                      </button>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+
+                  {item.status === "confirmed" ? (
+                    <div>
+                      <div>
+                        <p>
+                          Ghi chú:{" "}
+                          <span className="info-item">{item.note}</span>
+                        </p>
+                      </div>
+                      {new Date().getTime() < new Date(item.start).getTime()
+                        ? this.countdown(item.start)
+                        : ""}
+                      {new Date().getTime() > new Date(item.end).getTime()
+                        ? this.changeStatus(item._id)
+                        : ""}
+                      {!this.state.isShowCallButton ? (
+                        <p>
+                          Thời gian còn lại cho đến cuộc tư vấn:{" "}
+                          <span className="info-item">
+                            {this.state.hours}:{this.state.minutes}:
+                            {this.state.seconds}{" "}
+                          </span>
+                        </p>
+                      ) : (
+                        <button
+                          className="btn btn-success mt-1"
+                          onClick={(e) => this.call(item.linkCall)}
+                        >
+                          Bắt đầu cuộc gọi
+                        </button>
+                      )}
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  {item.status === "reject" ? (
+                    <p>
+                      Lý do từ chối:{" "}
+                      <span className="info-item">{item.reasonOfReject}</span>
+                    </p>
+                  ) : (
+                    ""
+                  )}
+                </div>
               </div>
-            ) : ""}
-            {item.status === 'reject' ? (<p>Lý do từ chối: <span className="info-item">{item.reasonOfReject}</span></p>) : ''}
-          </div>
-        </div>
-        ))) : ""}
-        <EditConsultation info_consultation={this.state.info_consultation} list_schedule={this.state.list_schedule}/>
-        <DeleteConsultation choice_delete={this.state.choice_delete}/>
+            ))
+          : ""}
+        <EditConsultation
+          info_consultation={this.state.info_consultation}
+          list_schedule={this.state.list_schedule}
+        />
+        <DeleteConsultation choice_delete={this.state.choice_delete} />
       </div>
     );
   }
